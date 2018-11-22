1a3871779161c35fd9ab45275380881443bbcd98
Joerg Tobi H <jhummler@gmail.com>
2018-11-22 09:58:44 +0100

    added CDI, JAXRS, Logging and a simple Rest Controller

---
+++ src/main/java/com/github/jaytobi/thorntail/jpa/Application.java
@@ -0,0 +1,8 @@
+package com.github.jaytobi.thorntail.jpa;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/")
+public class Application extends javax.ws.rs.core.Application {
+
+}
---
+++ src/main/java/com/github/jaytobi/thorntail/jpa/controller/SimpleRestController.java
@@ -0,0 +1,22 @@
+package com.github.jaytobi.thorntail.jpa.controller;
+
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.time.LocalDateTime;
+
+@RequestScoped
+@Path("/simple")
+public class SimpleRestController {
+    private static Logger log = Logger.getLogger(SimpleRestController.class);  //use this - because slf4j may cause problems on Java 9 + ?
+
+    @GET
+    @Path("/")
+    public String hello() {
+        LocalDateTime now = LocalDateTime.now();
+        log.warnf("now is %s", now);
+        return now.toString();
+    }
+}
